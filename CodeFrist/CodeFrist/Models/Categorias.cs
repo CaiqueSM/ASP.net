49eba52f04635dc9205aa2214acc6303640278aa
Caíque da Silva Machado <caique9machado@gmail.com>
2022-03-07 19:13:39 -0300

    Code-First
    Criação do modelo do banco de dados a partir das classes de objeto.

---
+++ CodeFrist/CodeFrist/Models/Categorias.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFrist.Models
+{
+    public class Categorias
+    {
+        [Key]
+        public int CategoriaID { get; set; }
+        public string Categoria { get; set; }
+        public string Descricao { get; set; }
+        public virtual ICollection<Posts> Posts { get; set; }
+    }
+
+    public class Posts
+    {
+        [Key]
+        public long PostID { get; set; }
+        public string TituloPost { get; set; }
+        public string ResumoPost { get; set; }
+        public string ConteudoPost { get; set; }
+        public DateTime DataPostagem { get; set; }
+        public int CategoriaID { get; set; }
+        [ForeignKey("CategoriaID")]
+        public virtual Categorias Categorias
+        {
+            get; set;
+        }
+    }
+
+    public class BlogContext : DbContext
+    {
+        public DbSet<Posts> Posts { get; set; }            
+        public DbSet<Categorias> Categorias { get; set; }
+    }
+
+
+}
---
+++ CodeFrist/CodeFrist/Models/CodeFristContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFrist.Models
+{
+    public class CodeFristContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CodeFristContext() : base("name=CodeFristContext")
+        {
+           
+        }
+
+        public System.Data.Entity.DbSet<CodeFrist.Models.Categorias> Categorias { get; set; }
+
+        public System.Data.Entity.DbSet<CodeFrist.Models.Posts> Posts { get; set; }
+    }
+}
