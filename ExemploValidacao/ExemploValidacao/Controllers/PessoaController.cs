fbebf028856a70db30c81edf8e54d7cc54a5c96a
Caíque da Silva Machado <caique9machado@gmail.com>
2022-03-07 19:25:36 -0300

    Exemplo de validação
    Realiza a validação dos campos de um formulário.

---
+++ ExemploValidacao/ExemploValidacao/Controllers/PessoaController.cs
@@ -0,0 +1,57 @@
+﻿using ExemploValidacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploValidacao.Controllers
+{
+    public class PessoaController : Controller
+    {
+        // GET: Pessoa
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            /*if (string.IsNullOrEmpty(pessoa.Nome))
+            {
+                ModelState.AddModelError("Nome", "O campo nome é Obrigatorio");
+            }
+
+            if (pessoa.Senha != pessoa.ConfirmarSenha)
+            {
+                ModelState.AddModelError("", "As senhas não conferem");
+            }
+            */
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var Banco = new Collection<string>
+            {
+                "Cleyton",
+                "Anderson",
+                "Renata"
+            };
+            return Json(Banco.All(x => x.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet); 
+        }
+    }
+}
---
+++ ExemploValidacao/ExemploValidacao/Models/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploValidacao.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage ="Nome deve ser preenchido")]
+        public string Nome { get; set; }
+
+        [StringLength(50,MinimumLength = 5, ErrorMessage =  "A observação deve ter entre 5 e 50 caracteres")]
+        [Required(ErrorMessage = "Preencha a obsservação")]
+        public string Observacao { get; set; }
+
+        [Range(18, 50, ErrorMessage = "A idade deve estar entre 18 e 50 anos")]
+        [Required(ErrorMessage ="Informe uma idade")]
+        public int Idade { get; set; }
+
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "O Email não é valido")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"[a-zA-Z]{5,15}",ErrorMessage ="O login deve possuir somente letres e ter entre 5 e 15 caracteres")]
+        [Required(ErrorMessage ="O login deve ser preechido")]
+        [Remote("LoginUnico","Pessoa",ErrorMessage ="Este nome de login já exite.")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "A senha deve ser informada")]
+        public string Senha { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string ConfirmarSenha { get; set; } 
+    }
+}
